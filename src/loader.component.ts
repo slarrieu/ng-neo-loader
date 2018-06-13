713cdf965403548dde17b96bbd80f5b817c11950
Sebastian Larrieu <slarrieu@neocomplexx.com>
2018-06-13 17:40:18 -0300

    New version for angular 5

--- src/loader.component.ts
+++ src/loader.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs/Subscription';
 import { LoaderService } from './loader.service';
 import { LoaderState } from './loader';
